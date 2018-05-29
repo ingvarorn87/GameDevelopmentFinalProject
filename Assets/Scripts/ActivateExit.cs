4ae78c814d7288012e1d35ee5307b6e705dade2b
ingvarorn87 <ingvarorn87@gmail.com>
2018-05-29 10:54:08 +0200

    Added respawn trigger

---
+++ Assets/Scripts/ActivateExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateExit : MonoBehaviour {
+
+    public GameObject FindtoExit;
+	private void OnTriggerEnter()
+    {
+        FindtoExit.SetActive(true);
+    }
+}
